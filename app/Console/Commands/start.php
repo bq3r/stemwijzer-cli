1d8d8974fbc8391ca686953c2d19b7cc8fa885de
r5q0 <bilalemailsss@gmail.com>
2024-08-29 21:42:18 +0200

    chore: Update start command with survey questions

--- app/Console/Commands/start.php
+++ app/Console/Commands/start.php
@@ -27,56 +27,56 @@ class start extends Command
     {
         $parties = [
             'PVV' => [
-            'score' => -18,
-            'name' => 'Partij voor de Vrijheid (PVV)',
+                'score' => -16,
+                'name' => 'Partij voor de Vrijheid (PVV)',
             ],
             'FvD' => [
-            'score' => -16,
-            'name' => 'Forum voor Democratie (FvD)',
+                'score' => -15,
+                'name' => 'Forum voor Democratie (FvD)',
             ],
             'JA21' => [
-            'score' => -14,
-            'name' => 'JA21',
+                'score' => -14,
+                'name' => 'JA21',
             ],
             'SGP' => [
-            'score' => -12,
-            'name' => 'Staatkundig Gereformeerde Partij (SGP)',
+                'score' => -12,
+                'name' => 'Staatkundig Gereformeerde Partij (SGP)',
             ],
             'VVD' => [
-            'score' => -8,
-            'name' => 'Volkspartij voor Vrijheid en Democratie (VVD)',
+                'score' => -8,
+                'name' => 'Volkspartij voor Vrijheid en Democratie (VVD)',
             ],
             'CDA' => [
-            'score' => -4,
-            'name' => 'Christen-Democratisch Appèl (CDA)',
+                'score' => -4,
+                'name' => 'Christen-Democratisch Appèl (CDA)',
             ],
             'D66' => [
-            'score' => 0,
-            'name' => 'Democraten 66 (D66)',
+                'score' => 0,
+                'name' => 'Democraten 66 (D66)',
             ],
             'CU' => [
-            'score' => 2,
-            'name' => 'ChristenUnie (CU)',
+                'score' => 2,
+                'name' => 'ChristenUnie (CU)',
             ],
             'PvdD' => [
-            'score' => 6,
-            'name' => 'Partij voor de Dieren (PvdD)',
+                'score' => 6,
+                'name' => 'Partij voor de Dieren (PvdD)',
             ],
             'PvdA' => [
-            'score' => 8,
-            'name' => 'Partij van de Arbeid (PvdA)',
+                'score' => 8,
+                'name' => 'Partij van de Arbeid (PvdA)',
             ],
             'SP' => [
-            'score' => 14,
-            'name' => 'Socialistische Partij (SP)',
+                'score' => 14,
+                'name' => 'Socialistische Partij (SP)',
             ],
             'GroenLinks' => [
-            'score' => 16,
-            'name' => 'GroenLinks',
+                'score' => 16,
+                'name' => 'GroenLinks',
             ],
             'DENK' => [
-            'score' => 10,
-            'name' => 'DENK',
+                'score' => 10,
+                'name' => 'DENK',
             ],
         ];
 
@@ -96,23 +96,94 @@ class start extends Command
                 'Ongewijzigd laten',
                 'Verhogen',
             ],
-            'Hoe kijk je naar belasting op grote bedrijven?' => [
+            'Wat vind je van de Europese Unie?' => [
+                'Minder macht naar de EU',
+                'EU-hervormingen, maar blijven',
+                'EU verder integreren',
+            ],
+            'Hoe belangrijk is milieubeleid voor jou?' => [
+                'Niet belangrijk',
+                'Neutraal',
+                'Belangrijk',
+            ],
+            'Wat is jouw standpunt over privatisering van openbare diensten?' => [
+                'Privatisering',
+                'Huidige situatie behouden',
+                'Nationaliseren',
+            ],
+            'Hoe zie je de rol van religie in de overheid?' => [
+                'Religie moet een rol spelen',
+                'Scheiding van kerk en staat',
+                'Religie moet geen rol spelen',
+            ],
+            'Wat is jouw mening over defensie-uitgaven?' => [
+                'Verhogen',
+                'Huidige niveau behouden',
                 'Verlagen',
-                'Ongewijzigd laten',
+            ],
+            'Hoe denk je over multiculturalisme?' => [
+                'Negatief',
+                'Neutraal',
+                'Positief',
+            ],
+            'Wat vind je van abortusrechten?' => [
+                'Strenger beperken',
+                'Behouden met beperkingen',
+                'Vrije keuze behouden',
+            ],
+            'Hoe kijk je naar de rol van de staat in de economie?' => [
+                'Minder staatsinterventie',
+                'Geringe staatsinterventie',
+                'Meer staatsinterventie',
+            ],
+            'Wat is jouw standpunt over klimaatverandering?' => [
+                'Geen probleem',
+                'Licht probleem',
+                'Ernstig probleem',
+            ],
+            'Hoe denk je over onderwijs?' => [
+                'Meer privatiseren',
+                'Huidige systeem behouden',
+                'Meer overheidsgestuurd maken',
+            ],
+            'Wat is jouw mening over belastingen voor de rijken?' => [
+                'Verlaging',
+                'Geen verandering',
                 'Verhogen',
             ],
-            'Hoe kijk je naar belasting op grote bedrijven?' => [
-                'Verlagen',
-                'Ongewijzigd laten',
-                'Verhogen',
+            'Hoe belangrijk is veiligheid voor jou?' => [
+                'Belangrijkste prioriteit',
+                'Zeer belangrijk',
+                'Minder belangrijk',
             ],
-            'Hoe kijk je naar belasting op grote bedrijven?' => [
-                'Verlagen',
-                'Ongewijzigd laten',
-                'Verhogen',
+            'Wat is jouw standpunt over LGBTQ+-rechten?' => [
+                'Beperken',
+                'Neutraal',
+                'Uitbreiden',
+            ],
+            'Hoe kijk je naar woningbouw?' => [
+                'Volledig door de markt laten bepalen',
+                'Gelijke rol voor privaat en publiek',
+                'Meer publiek dan privaat',
+            ],
+            'Wat vind je van vluchtelingenbeleid?' => [
+                'Grenzen sluiten',
+                'Strenger beleid',
+                'Ruimhartig toelaten',
+            ],
+            'Hoe denk je over economische ongelijkheid?' => [
+                'Natuurlijke uitkomst van de markt',
+                'Beperkte herverdeling',
+                'Actief herverdelen',
             ],
         ];
-
         
+
+        $count = 0;
+        foreach ($questionsAndAnswers as $question => $answers) {
+            $this->info($question);
+            $count++;
+        }
+        $this->info('Er zijn ' . $count . ' vragen.');
     }
 }
