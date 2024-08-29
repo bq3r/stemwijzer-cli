49049d7cbdb9327af8dc6a455449b194ef255505
r5q0 <bilalemailsss@gmail.com>
2024-08-29 21:15:14 +0200

    chore: Add start command to console commands

---
+++ app/Console/Commands/start.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class start extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'It starts the "stemwijzer';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
